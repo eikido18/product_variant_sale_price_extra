d6542cd60a75ec6b9b1395bc754d443887deefe1
eikido18 <145118939+eikido18@users.noreply.github.com>
2025-03-17 13:44:33 +0100

    Out of stock fix
    Update sale_variant_mixin.js

--- product_variant_sale_price_extra/static/src/js/sale_variant_mixin.js
+++ product_variant_sale_price_extra/static/src/js/sale_variant_mixin.js
@@ -1,10 +1,17 @@
+/** @odoo-module **/
+
 import { WebsiteSale } from '@website_sale/js/website_sale';
-import VariantMixin from '@website_sale/js/sale_variant_mixin';
+import VariantMixin from "@website_sale_stock/js/variant_mixin";
+import { renderToFragment } from "@web/core/utils/render";
+import { markup } from "@odoo/owl";
+import publicWidget from "@web/legacy/js/public/public_widget";
 
 const ChangeCombination = VariantMixin._onChangeCombination;
+const ChangeCombinationStock = VariantMixin._onChangeCombinationStock;
 
 VariantMixin._onChangeCombinationStock = function (ev, $parent, combination) {
     ChangeCombination.apply(this, arguments);
+    ChangeCombinationStock.apply(this, arguments);
     if (combination.has_discounted_price && (combination.compare_list_price || combination.compare_list_price === 0)) {
         var $compare_price = $(".oe_compare_list_price")
         $compare_price.find('.oe_currency_value').html(combination.compare_list_price);
@@ -19,10 +26,3 @@ VariantMixin._onChangeCombinationStock = function (ev, $parent, combination) {
         }
     }
 };
-
-
-WebsiteSale.include({
-
-    _onChangeCombination: VariantMixin._onChangeCombinationStock
-
-});
